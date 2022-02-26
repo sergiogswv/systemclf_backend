16a026e7e4bd2f59e356e7f30f9b8766610f9862
Sergio Guadarrama <sergio.gswv@outlook.com>
2022-02-26 13:08:10 -0600

    primer commit, se crea el proyecto con sus dependencias

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
